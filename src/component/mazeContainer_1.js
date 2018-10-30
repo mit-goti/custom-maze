792caf92b72f0f3de58335428cbef13364882ebc
mit-goti <mitgoti3@gmail.com>
2018-10-30 13:01:15 +0530

    zoomIn_zoomOut

--- src/component/mazeContainer_1.js
+++ src/component/mazeContainer_1.js
@@ -19,7 +19,8 @@ class Mazecontainer_1 extends Component {
       end: {x: 0 , y: 0},
       drag: false,
       w: false,
-      b: false 
+      b: false ,
+      height: 30
     };
 }
 
@@ -37,7 +38,7 @@ class Mazecontainer_1 extends Component {
                 temp_mat.push(0);
                 c++;
             }
-            this.state.maze.push(<div className="row" style={{width: "3000px"}}> 
+            this.state.maze.push(<div id = {i} className="row" style={{width: "3000px"}}> 
             {
                 temp.map(it => {return it;})
             }
@@ -142,6 +143,7 @@ class Mazecontainer_1 extends Component {
     }
 
     onClickStart() {
+
         var x = document.getElementById(this.state.start.x + "_" + this.state.start.y);
         x.style.backgroundColor = "white";
         this.setState({startClicked: 1 , endClicked: 0});
@@ -198,6 +200,28 @@ class Mazecontainer_1 extends Component {
         setTimeout(() => {this.dfsNextStep()} , 100);
     }
 
+    zoomIn() {
+        this.setState({height: this.state.height + 1});
+        for(var i = 0 ; i < 100 ; i++) {
+            document.getElementById(i).style.width = (this.state.height*100) + "px";
+            for(var j = 0 ; j < 100 ; j++) {
+                document.getElementById(i+"_"+j).style.height = this.state.height + "px";
+                document.getElementById(i+"_"+j).style.width = this.state.height + "px";
+            }
+        }
+    }
+
+    zoomOut() {
+        this.setState({height: this.state.height - 1});
+        for(var i = 0 ; i < 100 ; i++) {
+            document.getElementById(i).style.width = (this.state.height*100) + "px";
+            for(var j = 0 ; j < 100 ; j++) {
+                document.getElementById(i+"_"+j).style.height = this.state.height + "px";
+                document.getElementById(i+"_"+j).style.width = this.state.height + "px";
+            }
+        }
+    }
+
 
   render() {
     console.log(this.state.show+this.state.drag);
@@ -214,6 +238,14 @@ class Mazecontainer_1 extends Component {
         </div>
         <div style = {{position : "fixed" , height : "50px" , width : "50px" , borderRadius : "50px" , top : "90%" , left : "95%" , backgroundColor : "rgba(209, 125, 51, 0.9)" , color : "white" , textAlign: "center" , lineHeight: "50px" , fontWeight: "bold"}} onClick={this.onClickEnd.bind(this)}>
             End
+        </div>
+
+        <div style = {{position : "fixed" , height : "30px" , width : "30px" , borderRadius : "30px" , top : "85%" , left : "5%" , backgroundColor : "white" , color : "black" , textAlign: "center" , lineHeight: "30px" , fontWeight: "bold" , border: "1px solid black"}} onClick={this.zoomIn.bind(this)}>
+            +
+        </div>
+
+        <div style = {{position : "fixed" , height : "30px" , width : "30px" , borderRadius : "30px" , top : "90%" , left : "5%" , backgroundColor : "white" , color : "black" , textAlign: "center" , lineHeight: "30px" , fontWeight: "bold" , border: "1px solid black"}} onClick={this.zoomOut.bind(this)}>
+            -
         </div>
         <Draggable>
         <div className="block">
